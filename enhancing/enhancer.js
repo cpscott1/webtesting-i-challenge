f48956f3e8f9f31ea74083c2b3cdcf6075fb3b11
cpscott1 <thacam18@gmail.com>
2019-06-19 22:33:50 -0500

    finished mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,14 +6,26 @@ module.exports = {
 };
 
 function succeed(item) {
+  if (item.enhancement !== 20) {
+    item.enhancement +=1;
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability -=5
+  } else {
+    item.durability -=10
+  }
+  if(item.enhancement > 16) {
+    item.enhancement -= 1
+  }
   return { ...item };
 }
 
 function repair(item) {
+  item = { ...item, durability: 100 }
   return { ...item };
 }
 
